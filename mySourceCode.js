69b874626d8c9153e4478fea0ad00213101b8e17
mrcjssnr <mrcjssnr@gmail.com>
2024-02-22 06:38:24 +0800

    flash-like animation

--- mySourceCode.js
+++ mySourceCode.js
@@ -9,5 +9,13 @@ const nextSequence = function () {
   const randomNum = Math.floor(Math.random() * 4);
   const randomColor = buttonColors[randomNum];
   gamePattern.push(randomColor);
+
+  // PUT SOME FLASH-LIKE ANIMATION
+  document.querySelector("." + randomColor).classList.add("pressed");
+  setTimeout(function() {
+    document.querySelector("." + randomColor).classList.remove("pressed");
+  }, 300);
 }
 
+// Called the function
+nextSequence();
