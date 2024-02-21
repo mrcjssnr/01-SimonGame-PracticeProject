2226ab6d3e8f536c679e45f3a2f0654421a466ef
mrcjssnr <mrcjssnr@gmail.com>
2024-02-22 05:45:42 +0800

    added nextSequence function and some variables

--- mySourceCode.js
+++ mySourceCode.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const buttonColors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+
+
+
+const nextSequence = function () {
+  const randomNum = Math.floor(Math.random() * 4);
+  const randomColor = buttonColors[randomNum];
+  gamePattern.push(randomColor);
+}
+
