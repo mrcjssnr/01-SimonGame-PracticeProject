68d4baa95a9d921f89765e7b1634181d47c64b95
mrcjssnr <mrcjssnr@gmail.com>
2024-02-25 08:25:29 +0800

    added click event and restructured the code

--- mySourceCode.js
+++ mySourceCode.js
@@ -2,32 +2,43 @@
 
 const buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+let userClickedPattern = [];
 
-
-
-const nextSequence = function () {
+// HIGHER ORDER FUNCTION
+function nextSequence() {
   const randomNum = Math.floor(Math.random() * 4);
   const randomColor = buttonColors[randomNum];
   gamePattern.push(randomColor);
 
-  // PUT SOME FLASH-LIKE ANIMATION
-  document.querySelector("." + randomColor).classList.add("pressed");
-  setTimeout(function() {
-    document.querySelector("." + randomColor).classList.remove("pressed");
-  }, 300);
-
+  // called the function animateColor
+  animateColor(randomColor);
   // called the function playAudio
-  playAudio();
-}
-
-
-// Called the function nextSequence
-nextSequence();
-
-// Play audio FUNCTION
-function playAudio() {
-  const audioHtml = `<audio src=\"./sounds/green.mp3\" autoplay></audio>`;
-  const hi = document.querySelector(".btn").insertAdjacentHTML("afterbegin", audioHtml);
-  console.log(hi);
+  playAudio(randomColor);
 };
 
+// CALLBACK FUNCTIONS - PLAY AUDIO, ANIMATE COLOR
+function playAudio(color) {
+  const audioHtml = `<audio src=\"./sounds/${color}.mp3\" autoplay></audio>`;
+  const hi = document
+    .querySelector(".btn")
+    .insertAdjacentHTML("afterbegin", audioHtml);
+  console.log(document.querySelector(".btn"));
+};
+
+function animateColor(color) {
+  document.querySelector("." + color).classList.add("pressed");
+  setTimeout(function () {
+    document.querySelector("." + color).classList.remove("pressed");
+  }, 300);
+};
+
+
+//EVENT HANDLERS
+const btn = document.querySelectorAll(".btn");
+btn.forEach(e => e.addEventListener("click", function(ev) {
+  const chosenColor = ev.target.id;
+  userClickedPattern.push(chosenColor);
+
+  playAudio(chosenColor);
+  animateColor(chosenColor);
+}));
