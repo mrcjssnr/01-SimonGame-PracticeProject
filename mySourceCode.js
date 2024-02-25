906fb8ed8cb25e0724d4f2a493d1b477f0ef4c02
mrcjssnr <mrcjssnr@gmail.com>
2024-02-25 18:32:52 +0800

    added keypress event, check answer function, and start over function

--- mySourceCode.js
+++ mySourceCode.js
@@ -3,18 +3,24 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
+let level = 0;
+let start = true;
 
 // HIGHER ORDER FUNCTION
 function nextSequence() {
   const randomNum = Math.floor(Math.random() * 4);
   const randomColor = buttonColors[randomNum];
+  userClickedPattern = [];
   gamePattern.push(randomColor);
+  console.log(gamePattern);
+  level++;
+  document.querySelector("#level-title").textContent = `level ${level}`;
 
   // called the function animateColor
   animateColor(randomColor);
   // called the function playAudio
   playAudio(randomColor);
-};
+}
 
 // CALLBACK FUNCTIONS - PLAY AUDIO, ANIMATE COLOR
 function playAudio(color) {
@@ -22,23 +28,52 @@ function playAudio(color) {
   const hi = document
     .querySelector(".btn")
     .insertAdjacentHTML("afterbegin", audioHtml);
-  console.log(document.querySelector(".btn"));
-};
+}
 
 function animateColor(color) {
   document.querySelector("." + color).classList.add("pressed");
   setTimeout(function () {
     document.querySelector("." + color).classList.remove("pressed");
   }, 300);
-};
+}
 
+function checkAnswer(currentLevel) {
+  // if the answer is correct proceed to next sequence
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    // if the answer is wrong or if the clicked button is not the same as the gamePattern, these codes will be executed
+    playAudio("wrong");
+    document.querySelector("#level-title").textContent = "Game Over, Press Any Key to Restart";
+    
+    document.querySelector("body").classList.add("game-over");
+    setTimeout(function() {
+      document.querySelector("body").classList.remove("game-over");
+    }, 300)
+  }
+}
 
-//EVENT HANDLERS
-const btn = document.querySelectorAll(".btn");
-btn.forEach(e => e.addEventListener("click", function(ev) {
-  const chosenColor = ev.target.id;
-  userClickedPattern.push(chosenColor);
+// --------- EVENT HANDLERS -------- //
+// Added click event
+document.querySelectorAll(".btn").forEach((e) =>
+  e.addEventListener("click", function (ev) {
+    const chosenColor = ev.target.id;
+    userClickedPattern.push(chosenColor);
 
-  playAudio(chosenColor);
-  animateColor(chosenColor);
-}));
+    playAudio(chosenColor);
+    animateColor(chosenColor);
+    checkAnswer(userClickedPattern.length - 1);
+  })
+);
+// Added keypress event
+document.addEventListener("keypress", function (ev) {
+  if (ev.key === "a" && start) {
+    console.log("you pressed the right key");
+    start = false;
+    nextSequence();
+  }
+});
